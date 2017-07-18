1bfdf1a60669db5724e80c4547850f05c57f681e
khusika <khusikadhamar@gmail.com>
2017-07-18 12:16:32 +0700

    Add Scanner

--- app/src/main/java/com/pit/qrcodesrajabrawijaya/MainActivity.java
+++ app/src/main/java/com/pit/qrcodesrajabrawijaya/MainActivity.java
@@ -1,17 +1,33 @@
 package com.pit.qrcodesrajabrawijaya;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.pit.qrcodesrajabrawijaya.activities.AboutActivity;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -19,6 +35,11 @@ public class MainActivity extends AppCompatActivity
     private ActionBarDrawerToggle toggle;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+
+    DatabaseHandler db = new DatabaseHandler(this);
+
+    private static final int REQUEST_RUNTIME_PERMISSION = 123;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +56,21 @@ public class MainActivity extends AppCompatActivity
 
         navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        /**
+         * CRUD Operations
+         * */
+
+        // Reading all contacts
+        Log.d("Reading: ", "Reading all contacts..");
+        List<Absensi> absensi = db.getAllAbsensi();
+
+        for (Absensi cn : absensi) {
+            String log = "Id: "+cn.getID()+" , NIM: " + cn.getNim() + " , WAKTU: " + cn.getWaktu();
+            // Writing Absensis to log
+            Log.d("NIM: ", log);
+        }
+
     }
 
     @Override
@@ -76,4 +112,80 @@ public class MainActivity extends AppCompatActivity
         }
         return false;
     }
+
+    public void mulaiScan() {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setOrientationLocked(true);
+        integrator.initiateScan();
+    }
+
+    private String getTanggal() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                if(db.cekNim(result.getContents()) == 0) {
+                    db.addAbsensi(new Absensi(result.getContents(), getTanggal()));
+                }
+                mulaiScan();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    public void scanAbsensi(View view){
+        if (CheckPermission(MainActivity.this, Manifest.permission.CAMERA)) {
+            // you have permission go ahead
+            mulaiScan();
+        } else {
+            // you do not have permission go request runtime permissions
+            RequestPermission(MainActivity.this, Manifest.permission.CAMERA, REQUEST_RUNTIME_PERMISSION);
+        }
+    }
+
+    public boolean CheckPermission(Context context, String Permission) {
+        if (ContextCompat.checkSelfPermission(context, Permission) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void RequestPermission(Activity thisActivity, String Permission, int Code) {
+        if (ContextCompat.checkSelfPermission(thisActivity, Permission) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(thisActivity, Permission)) {
+            } else {
+                ActivityCompat.requestPermissions(thisActivity, new String[]{Permission}, Code);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int permsRequestCode, String[] permissions, int[] grantResults) {
+
+        switch (permsRequestCode) {
+
+            case REQUEST_RUNTIME_PERMISSION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // you have permission go ahead
+                    mulaiScan();
+                } else {
+                    Toast.makeText(getApplicationContext(),"Error! Kamu Harus Mengizinkan Aplikasi mengakses kamera!",Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+
 }
